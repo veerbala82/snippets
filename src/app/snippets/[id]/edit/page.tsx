5019a4f92df12eee981360ec92ab4b9f288a7be1
Dovetail <balajiv@dovetail.co.za>
2024-04-19 20:06:17 +0530

    Initial Commit

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,13 @@
+
+interface SnippetEditPageProps {
+    params: {
+        id: string
+    }
+}
+
+export default function SnippetEditPage(props: SnippetEditPageProps) {
+    const id = parseInt(props.params.id);
+
+    return <div>Editing snippet with id {id}</div>
+
+}
